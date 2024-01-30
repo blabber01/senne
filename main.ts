21d70cc1de9abdc5eab6d5299d1f3b642f01bced
blabber01 <156662034+blabber01@users.noreply.github.com>
2024-01-30 16:06:25 +0100

    stoplicht

--- main.ts
+++ main.ts
@@ -1,9 +1,22 @@
-let getal = 0
+input.onButtonPressed(Button.B, function () {
+    Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.On)
+    Kitronik_STOPbit.trafficLightState(Kitronik_STOPbit.LightStates.GetReady)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(100)
+    Kitronik_STOPbit.trafficLightState(Kitronik_STOPbit.LightStates.Go)
+    basic.pause(5000)
+    basic.showIcon(IconNames.No)
+    Kitronik_STOPbit.trafficLightState(Kitronik_STOPbit.LightStates.Stop)
+    Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.On)
+})
+Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.On)
+basic.showIcon(IconNames.No)
 basic.forever(function () {
-    if (getal < 36912) {
-        basic.showNumber(getal)
-    } else {
-        getal += 3
-        basic.pause(1000)
-    }
+	
 })
