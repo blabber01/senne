0d692cedf09d3dea4e6a7fa9ecc4ca0f7cda1c6c
blabber01 <156662034+blabber01@users.noreply.github.com>
2024-02-05 11:31:27 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,22 +1,5 @@
-input.onButtonPressed(Button.B, function () {
-    Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.On)
-    Kitronik_STOPbit.trafficLightState(Kitronik_STOPbit.LightStates.GetReady)
-    basic.showLeds(`
-        . . # . .
-        . # # # .
-        # . # . #
-        . . # . .
-        . . # . .
-        `)
-    basic.pause(100)
-    Kitronik_STOPbit.trafficLightState(Kitronik_STOPbit.LightStates.Go)
-    basic.pause(5000)
-    basic.showIcon(IconNames.No)
-    Kitronik_STOPbit.trafficLightState(Kitronik_STOPbit.LightStates.Stop)
-    Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.On)
-})
-Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.On)
-basic.showIcon(IconNames.No)
-basic.forever(function () {
-	
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(randint(1, 6))
+    basic.pause(2000)
+    basic.clearScreen()
 })
