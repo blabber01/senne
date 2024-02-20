9be603216f2e4e61cefb4c29618231d1776f227a
blabber01 <156662034+blabber01@users.noreply.github.com>
2024-02-20 16:06:18 +0100

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,21 +1,36 @@
-input.onGesture(Gesture.LogoDown, function () {
-	
-})
-input.onGesture(Gesture.Shake, function () {
-	
-})
-input.onGesture(Gesture.LogoUp, function () {
-	
-})
-let kompas = input.compassHeading()
 basic.forever(function () {
-    if (kompas > 315) {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Left) || Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Right)) {
         basic.showLeds(`
             . . # . .
+            . # # . .
+            . . # . .
+            . . # . .
             . # # # .
-            # . # . #
-            . . # . .
-            . . # . .
             `)
+    } else {
+        if (!(Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Right))) {
+            basic.showLeds(`
+                . # # . .
+                # . . # .
+                # . . # .
+                # . . # .
+                . # # . .
+                `)
+        }
+        if (!(Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Left))) {
+            basic.showLeds(`
+                . # # . .
+                # . . # .
+                # . . # .
+                # . . # .
+                . # # . .
+                `)
+        }
     }
 })
