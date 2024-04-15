a9febdc062dfb83c46911fa92bcf61e25b802d32
blabber01 <156662034+blabber01@users.noreply.github.com>
2024-04-15 11:30:49 +0200

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,6 +1,63 @@
 let ir_waarde = 0
+let vooruit = 94
+let achteruit = 95
+let rechts = 92
+let Links = 93
 basic.forever(function () {
-    basic.showNumber(ir_waarde)
+    let stop = 0
+    if (ir_waarde == vooruit) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+    }
+    if (ir_waarde == achteruit) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
+    }
+    if (ir_waarde == rechts) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 27)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 27)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
+    }
+    if (ir_waarde == Links) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 27)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 27)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
+    }
+    if (ir_waarde == stop) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+            `)
+    }
 })
 basic.forever(function () {
     ir_waarde = IR.IR_read()
