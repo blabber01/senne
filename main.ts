b84685281fbe257f899683251aecf82559904705
blabber01 <156662034+blabber01@users.noreply.github.com>
2024-04-22 11:32:15 +0200

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,10 +1,14 @@
 let ir_waarde = 0
+let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 let vooruit = 94
 let achteruit = 95
 let rechts = 92
 let Links = 93
+let stop = 78
 basic.forever(function () {
-    let stop = 0
+    ir_waarde = IR.IR_read()
+})
+basic.forever(function () {
     if (ir_waarde == vooruit) {
         basic.showLeds(`
             . . # . .
@@ -14,6 +18,7 @@ basic.forever(function () {
             . . # . .
             `)
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
     }
     if (ir_waarde == achteruit) {
         basic.showLeds(`
@@ -24,6 +29,7 @@ basic.forever(function () {
             . . # . .
             `)
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
+        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
     }
     if (ir_waarde == rechts) {
         basic.showLeds(`
@@ -33,9 +39,9 @@ basic.forever(function () {
             . . . # .
             . . # . .
             `)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 27)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 27)
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 56)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 53)
+        strip.showColor(neopixel.colors(NeoPixelColors.Purple))
     }
     if (ir_waarde == Links) {
         basic.showLeds(`
@@ -45,20 +51,19 @@ basic.forever(function () {
             . # . . .
             . . # . .
             `)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 27)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 27)
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 56)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 53)
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
     }
     if (ir_waarde == stop) {
         basic.showLeds(`
             . . . . .
             . # # # .
-            . # . # .
+            . # # # .
             . # # # .
             . . . . .
             `)
+        maqueen.motorStop(maqueen.Motors.All)
+        strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
     }
-})
-basic.forever(function () {
-    ir_waarde = IR.IR_read()
 })
